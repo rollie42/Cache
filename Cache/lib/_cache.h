2438eab1023df0db53f16dc5c82ca04becbf1382
rollie42 <github@rolnicki.net>
2017-04-25 13:02:37 +0900

    Syntax updates

--- Cache/lib/_cache.h
+++ Cache/lib/_cache.h
@@ -9,17 +9,17 @@ namespace cache
 	using std::unordered_map;
 
 	// template class for all cache types; allows specification of a replacement policy
-	template <typename ValueT, typename ReplP, typename KeyT = string>
+	template <typename ValueT, typename ReplPolicyT, typename KeyT = string>
 	class cache
 	{
-		ReplP m_replacementPolicy;
+		ReplPolicyT m_replacementPolicy;
 
 		unordered_map<KeyT, ValueT> m_data;
 		size_t m_maxSize;
 
 	public:
 		cache(size_t _maxSize = 1000)
-			: m_data(_maxSize + 1),
+			: m_data(_maxSize),
 			m_maxSize(_maxSize),
 			m_replacementPolicy(_maxSize)
 		{
@@ -27,18 +27,26 @@ namespace cache
 
 		void add(const KeyT & _key, const ValueT & _val)
 		{
-			m_replacementPolicy.add(_key);
-			m_data[_key] = _val;
-			if (m_data.size() > m_maxSize)
+			if (has(_key))
+			{
+				m_data[_key] = _val;
+				m_replacementPolicy.get(_key); // treat modification of the value for a given key like a 'get' operation
+				return;
+			}
+
+			if (m_data.size() >= m_maxSize)
 			{
 				m_data.erase(m_replacementPolicy.remove());
 			}
+
+			m_replacementPolicy.add(_key);
+			m_data[_key] = _val;
 		}
 
 		ValueT & get(const KeyT & _key)
 		{
 			m_replacementPolicy.get(_key);
-			return m_data.at(_key);
+			return m_data[_key];
 		}
 
 		bool has(const KeyT & _key) const
@@ -48,7 +56,7 @@ namespace cache
 
 		ValueT & operator[](const KeyT & _key)
 		{
-			return this->get(_key);
+			return get(_key);
 		}
 
 		auto begin()
